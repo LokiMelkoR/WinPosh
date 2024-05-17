206ce37a9f00d939e12bd8e1b3653cd444a8a669
Malith Madushanka <malith.pitiwilaliyanage@medel.com>
2024-05-17 10:58:18 +0200

    Added and cleaned the powershell to a if loop

--- VDI QA/WindowsBloatware.ps1
+++ VDI QA/WindowsBloatware.ps1
@@ -1,7 +1,6 @@
-Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like 'Microsoft.Xbox*'}
-Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like 'Microsoft.Xbox*'} |Remove-AppxProvisionedPackage -AllUsers -Online
+# This is to clean up some of the nightmare windows apps you would likely not need in your corporate VDI solution
 
-if(Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like 'Microsoft.Xbox*'}){
+if(Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like 'Microsoft.Xbox*'}|Remove-AppxProvisionedPackage -AllUsers -Online){
     Write-Host "Microsft Xbox app removed succesfully"
 }
   else{
